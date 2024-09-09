4ddbbddb5e8aae4f5ff357197384c906c0919eb1
Milwad <milwad.dev@gmail.com>
2024-09-09 19:11:10 +0330

    Update 2023_07_23_155120_create_products_table.php

--- tests/SetUp/Migrations/2023_07_23_155120_create_products_table.php
+++ tests/SetUp/Migrations/2023_07_23_155120_create_products_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
             $table->string('title');
             $table->text('description')->nullable();
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
